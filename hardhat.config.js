8e2702b6bfd5e8b59a536ebbe1b43be0890205da
Endi Hariadi <59972539+EndiHariadi43@users.noreply.github.com>
2025-09-05 17:57:02 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,19 +4,20 @@ module.exports = {
   solidity: {
     version: "0.8.20",
     settings: {
-      optimizer: { enabled: true, runs: 200 },
+      optimizer: { enabled: false, runs: 200 }, // sama: optimizer OFF
+      viaIR: false,                              // default Remix
       evmVersion: "london",
-    },
+      metadata: { bytecodeHash: "ipfs" }        // default Remix/solc
+    }
   },
   networks: {
-    bsc: { url: "https://bsc-dataseed.binance.org" },
-    bscTestnet: { url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545" },
+    bsc: { url: "https://bsc-dataseed.binance.org", accounts: [process.env.PRIVATE_KEY] },
+    bscTestnet: { url: "https://data-seed-prebsc-1-s1.binance.org:8545", accounts: [process.env.PRIVATE_KEY] }
   },
   etherscan: {
-    apiKey: process.env.BSCSCAN_API_KEY,
-    customChains: [
-      { network: "bsc", chainId: 56, urls: { apiURL: "https://api.bscscan.com/api", browserURL: "https://bscscan.com" } },
-      { network: "bscTestnet", chainId: 97, urls: { apiURL: "https://api-testnet.bscscan.com/api", browserURL: "https://testnet.bscscan.com" } }
-    ],
-  },
+    apiKey: {
+      bsc: process.env.BSCSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+    }
+  }
 };
